9adc7471d8df45bcfc613f22a99dda56406370b0
reoxb <suarzs@gmail.com>
2019-10-23 13:16:28 -0500

    set profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
-function App() {
+export default function (args) {
+  const initProfile = {
+    name : 'Juan', 
+    lastName: 'Suarez'
+  }
+
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [profile, setProfile] = useState(initProfile)
+
+  useEffect(() => {
+    localStorage.setItem('profile', JSON.stringify(profile));
+  }, [profile]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='name'></input>
+      <input type='text' value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder='last name'></input>
+      <button onClick={() => setProfile({name, lastName})}>Update profile</button>
+      <p>User info: {JSON.stringify(profile)}</p>
     </div>
-  );
+  )
 }
-
-export default App;
